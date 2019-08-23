ac04d5baf2b3318f35dbada90862d1aaca47724a
vanercarmo@gmail.com <vaner.carmo@oletecnologia.com.br>
2019-08-23 16:19:15 -0300

    Arquivo querys alterado

--- GestaoTributos.Data/Queries.cs
+++ GestaoTributos.Data/Queries.cs
@@ -8,7 +8,6 @@
         public const string ALTERAR_STATUS_LANCAMENTO_CONTABIL = "[dbo].[SP_ALTERAR_STATUS_LANCAMENTO_CONTABIL]";
         public const string BUSCAR_LANCAMENTO_CONTABIL = "[dbo].[SP_BUSCAR_LANCAMENTOS_CONTABEIS]";
         public const string INSERIR_LOG_LANCAMENTO_CONTABIL = "[dbo].[SP_INSERIR_LOG_LANCAMENTO_CONTABIL]";
-
         public const string BUSCAR_PARAMETRO = "[dbo].[SP_BUSCAR_PARAMETRO]";
     }
 }
